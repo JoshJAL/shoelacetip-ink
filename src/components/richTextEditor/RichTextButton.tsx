d108dbb9d4a8fd9416652343e35b584b806b29d9
JoshJAL <JoshuaALevine3@gmail.com>
2023-04-01 13:11:17 -0400

    Update RichTextButton.tsx to type button

--- src/components/richTextEditor/RichTextButton.tsx
+++ src/components/richTextEditor/RichTextButton.tsx
@@ -8,6 +8,7 @@ interface Props {
 export default function RichTextButton({ onClick, disabled, children, additionalClasses }: Props) {
   return (
     <button
+      type='button'
       onClick={onClick}
       disabled={disabled}
       className={`font-semibold bg-lilac px-2 py-1 hover:bg-lilacHover ${additionalClasses ? additionalClasses : ''}`}
